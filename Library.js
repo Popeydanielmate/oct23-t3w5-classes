87602c9be031b892f66b1158dd54bd5e388490ac
Popeydanielmate <14269@coderacademy.edu.au>
2024-06-09 15:16:15 +1000

    feat: class examples

---
+++ Library.js
@@ -0,0 +1,15 @@
+
+
+class Library {
+    constructor(){
+
+        this.books = [];
+        this.audiobooks = [];
+        this.games = [];
+        this.movies = [];
+        this.music = [];
+    }
+
+}
+
+module.exports = {Library}
---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+// Import Library class, Book class, Audiobook class, Games class, Movies class, Music class 
+
+const { Media } = require("./Media");
+
+// Create instances of classes 
+
+let lotr = new Media("fellowship of the ring");
+
+console.log(lotr);
+
+// Lord of the Rings as a instance of Book class 
+
+// Do things with those instances 
+
+// Create an instance of a Library
+// add media instances into the Library instance
+
+// Log the data in the library 
+
+// ./Users/Popeymate/Desktop/Projects/oct23-t3w5-classes/Media
