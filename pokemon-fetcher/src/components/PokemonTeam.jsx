7d7430fd347c5adfc9d4b84246f7fbd626dce47b
Popeydanielmate <14269@coderacademy.edu.au>
2024-06-10 13:21:55 +1000

    fix: make 6 requests instead of 1

--- pokemon-fetcher/src/components/PokemonTeam.jsx
+++ pokemon-fetcher/src/components/PokemonTeam.jsx
@@ -22,23 +22,31 @@ export default class PokemonTeam extends React.Component{
             return Math.floor(Math.random() * 1025) + 1;
     }
 
+    for (let index = 0; index < 6; index++){
         let randomNumber = getRandomPokemonId();
         console.log(randomNumber);
 
         let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + randomNumber).catch(error => {
-			console.log(error);
-			return error;
-		});
+            console.log(error);
+            return error;
+        });
         let data = await response.json();
-		console.log(data.name);
+        console.log(data.name);
 
         // Invalid way to update state:
         // this.state.pokemonTeam.push(data)
         // Valid way to update state:
-        this.setState({PokemonTeam: [...this.state.PokemonTeam, data]});
+        this.setState({pokemonTeam: [...this.state.pokemonTeam, data]});
+        // this.setState((state) => {
+        // 	let stateCopy = JSON.parse(JSON.stringify(this.state.pokemonTeam));
+        // 	let newTeam = [...stateCopy, data];
+
+        // 	return {pokemonTeam: newTeam}
+        // });
 
         // console.log("UUID example");
         // console.log(crypto.randomUUID());
+    }
     }
 
     
